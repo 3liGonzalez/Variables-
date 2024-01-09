31e627d31546afba6dcbec15579f8e5de7050be5
3liGonzalez <153216141+3liGonzalez@users.noreply.github.com>
2024-01-08 19:23:53 -0600

    Add files via upload

---
+++ variablesrepo.js
@@ -0,0 +1,69 @@
+/**
+ * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+ * Pueden variar entre las palabras reservadas var, let y const
+ *      -number
+ *      -string
+ *      -boolean
+ * 
+ * Declarar variables numericas y realizar las operaciones aritmeticas basicas
+ *      -suma
+ *      -resta
+ *      -multiplicacion
+ *      -division
+ * 
+ * Declarar variables que concatenen cadenas de texto
+ *      -5 mensajes diferentes en pantalla, sin limite de palabras.
+ * 
+ * Declarar 5 variables y convertirlas a otro tipo de dato
+ * 
+ *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+ * Imprimir los resultados en pantalla con un console.log()
+ */
+
+//variables tipo number
+let numeroEncuesta = 3; 
+let integrantesVivienda =5; 
+let numeroHabitaciones= 7;
+let mayoresDeEdad = 5;
+let totalPisos = 2; 
+let totalVentanas = 8; 
+let totalMascotas = 2; 
+var numeroDeFocos = 15; 
+var numeroDeCoches = 0; 
+let totalTelefonos = 4;
+
+
+//variable tipo booleano 
+var noSoyRobot = true; 
+let expertaEnJava = false;
+var soyMujer= true; 
+let inetresEnAprender = true;
+var vivoFeliz = true; 
+let trabajoEnEquipo = true;
+var soyHombre = false; 
+let cumplíObjetivos= true; 
+var nofumo= true; 
+let estudioMucho = true; 
+
+//variable tipo string 
+let mejorDia= "sabado"
+let frioOCalor = "frio"
+let dulceOSalado = "salado"
+let jamaicaUHorchata = "jamaica"
+let cancionFavorita = "luna"
+let comidaIdeal = "tacos"
+let mejorApp = "instagram" 
+let marcaDeComputadora = "Mac"
+let florFavorita = "lili"
+let cursoNuevo = "javascript"
+
+const LISTA_PALABRAS = "gustos"
+
+// variables numericas 
+let suma: (4 + 6);
+let resta: (20 - 13);
+let multiplicacion: (4 * 3); 
+let division: (12/3);
+
+
+// variables concatenadas 
