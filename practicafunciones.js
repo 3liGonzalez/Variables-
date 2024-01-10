c378dc7e743ef85527108d545ae983f448f426f3
3liGonzalez <153216141+3liGonzalez@users.noreply.github.com>
2024-01-09 20:11:33 -0600

    Add files via upload

---
+++ practicafunciones.js
@@ -0,0 +1,58 @@
+// Funciones
+
+/**
+ * 
+ * Declaración de funciones
+ * 
+ * 1. Palabra reservada function
+ * 2. Asignar un nombre a nuestra funcion
+ * 3. Colocar parentesis y despues llaves
+ * 
+ */
+/**Ejercicios para esta sesion
+ 
+ * CONSIDERACIONES: poner en practica las mejores practicas para declarar funciones
+ * Los ejercicios se entregan tanto en funciones con nombre como funciones flecha
+ */
+// calcular el perímetro del círculo 
+function calcularPerimetro (π,d) {
+    return "El perimetro calculado es de"; π * d;
+}
+// Calcular el perimetro del circulo con funciones flecha
+const perimetroFlecha = (π,d) => π * d;
+
+// calcular el área de un rectángulo 
+function calcularArea (b,h) {
+    return "El area calculada es "; b * h;
+}
+// calcular el área de un rectángulo con funciones flecha
+const areaFlecha = (b,h) => b * h;
+
+// calcular el cuadrado de un número
+function calcularCuadradoNumero (n,n) {
+    return "El cuadrado de un numero es"; n * n;
+}
+// calcular el cuadrado de un número con funciones flecha
+const cuadradoFlecha = (n,n) => n * n;
+//calcular la conversion de grados celsius a farenheit
+function convertirGrados ( ºF = (ºC * 1,8) + 32) {
+    return "La conversion es"; ºF = (ºC * 1,8) + 32;
+}
+//calcular la conversion de grados celsius a farenheit con funcion flecha
+const conversionGradosFlecha =  ( ºF = (ºC * 1,8) + 32) => ºF = (ºC * 1,8) + 32;
+
+//calcular el valor del voltaje dadas la resistencia y la corriente
+function valorVoltaje (E = I*R ) {
+    return "El valor del voltaje es"; (E = I*R );
+}
+//calcular el valor del voltaje dadas la resistencia y la corriente funcion flecha 
+const valorVoltajeFlecha = (E = I*R ) => (E = I*R );
+
+//calcular el volumen de una esfera
+function calcularVolumen (V = 4/3* π * r){
+    return "El valor del voltaje es"; (V = 4/3* π * r);
+} 
+//calcular el volumen de una esfera con función flecha 
+const calcularVolumenFlecha = (V = 4/3* π * r) =>(V = 4/3* π * r);
+ 
+
